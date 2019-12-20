fdf6512e6639eda960765e4610aa9844faecbc61
NaveedG <nganjani@protonmail.com>
2019-12-20 09:43:21 -0600

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -9,14 +9,14 @@ function reducer(state = {count: 0}, action){
   }
 }
  
-function dispatch(action){
-  state = reducer(state, action)
-  render()
-}
- 
 function render(){
   let container = document.getElementById('container');
   container.textContent = state.count;
+}
+
+function dispatch(action){
+  state = reducer(state, action)
+  render()
 }
 
 let button = document.getElementById('button');
